1a118163d99389393207ae5b3f6daadefae6ecfd
Ubuntu <ubuntu@ip-172-31-23-173.us-east-2.compute.internal>
2022-06-26 14:47:49 +0000

    updating script

--- transform_dataset.py
+++ transform_dataset.py
@@ -52,7 +52,8 @@ CATEGORIES = [{"supercategory": "box", "id": 1, "name": "box"}]
 
 def main():
     for folder in os.listdir(ORIGIN_PATH):
-
+        print(f"Getting into {folder}")
+        continue
         path = f"{ORIGIN_PATH}/{folder}/images"
         images = [
             {"id": image.replace(".jpg", ""), "file_name": image}
@@ -67,29 +68,33 @@ def main():
             ) as annotations_file:
                 image_annotations = json.load(annotations_file)
                 bboxes = image_annotations.get("bboxes", [])[0]
-                width = bboxes[2]
-                height = bboxes[3]
-
+                _, _, bbox_width, bbox_height = bboxes
+                # bbox_width = xmax - xmin + 1
+                # bbox_height = ymax - ymin + 1
+                if folder == "test":
+                    image_id = int.from_bytes(image_id.encode(), "big")
                 image_annotations.update(
                     {
                         "image_id": image_id,
                         "id": image_id,
                         "category_id": 1,
-                        "width": width,
-                        "height": height,
-                        "area" : width * height,
-                        "iscrowd" : 0,
-                        "bbox": image_annotations.get("bboxes", [])[0],
+                        "width": bbox_width,
+                        "height": bbox_height,
+                        "area": bbox_width * bbox_height,
+                        "iscrowd": 0,
+                        "bbox": bboxes,
                     }
                 )
                 annotations.append(image_annotations)
-                images_info.append({
-                    "id" : image_id,
-                    "licence" : 1,
-                    "file_name" : image.get("file_name"),
-                    "width": width,
-                    "height": height,
-                })
+                images_info.append(
+                    {
+                        "id": image_id,
+                        "licence": 1,
+                        "file_name": image.get("file_name"),
+                        "width": bbox_width,
+                        "height": bbox_height,
+                    }
+                )
 
         annotations = {
             "licenses": LICENCES,
@@ -100,6 +105,10 @@ def main():
         out_path = f"{DESTINATION_PATH}/instances_{folder}2017.json"
         with open(out_path, "w") as save_file:
             json.dump(annotations, save_file)
+        if folder == "test":
+            out_path = f"{DESTINATION_PATH}/instances_val2017.json"
+            with open(out_path, "w") as save_file:
+                json.dump(annotations, save_file)
 
 
 if __name__ == "__main__":
